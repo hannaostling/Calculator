7b3082266ad8948baa17f8f39bd1d49aff187059
Hanna Östling <hannaoliviaostling@gmail.com>
2018-11-19 18:24:23 +0100

    Initial commit

---
+++ Calculator/Calculator.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Calculator
+{
+    public partial class Calculator : ContentPage
+    {
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+    }
+}
